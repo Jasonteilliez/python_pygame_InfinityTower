0f5c4517863fb2add81dda1df3d215ea1f2a68d6
Jason Teilliez <jason.teilliez@gmail.com>
2025-01-29 16:46:03 +0100

    overlap

--- game/tile.py
+++ game/tile.py
@@ -5,4 +5,7 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/wall/wall64.png').convert_alpha()
+
+
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,0)
